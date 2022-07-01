839cba62f82048069a85d7d99f9c120555259dcd
Njabulo Mbatha <njabulo.mbatha@kcpivot.co.za>
2022-07-01 11:08:04 +0200

    first commit

---
+++ CodeWarsKata/Services/Katas.cs
@@ -0,0 +1,48 @@
+﻿
+namespace CodeWarsKata
+{
+    public class Katas
+    {
+        public int FindShort(string sentence)
+        {
+            string[] wordsArray = sentence.Split(' ');
+            string smallestWord = wordsArray[0];
+
+            foreach (var word in wordsArray)
+            {
+                if (smallestWord.Length > word.Length)
+                {
+                    smallestWord = word;
+                }
+            }
+
+            return smallestWord.Length;
+        }
+        public int FindShortTwo(string s)
+        {
+            return s.Split(' ').Min(x => x.Length);
+        }
+        public int FindShortArrowMethod(string s) => s.Split(' ').Min(t => t.Length);
+        public int FindShortLinq(string str)
+        {
+            return str.Split(' ').Select(elem => elem.Length).Min();
+        }
+
+        //
+
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            for (int index = 0; index < numbers.Length; index++)
+            {
+                for (int numberIndex = 1; numberIndex < numbers.Length; numberIndex++)
+                {
+                    if (numbers[index] + numbers[numberIndex] == target)
+                    {
+                        return new int[] { index, numberIndex };
+                    }
+                }
+            }
+            return new int[0];
+        }
+    }
+}
---
+++ CodeWarsKataTest/KatasTest.cs
@@ -0,0 +1,91 @@
+﻿
+using CodeWarsKata;
+
+namespace CodeWarsKataTest
+{
+    public class KatasTest
+    {
+        private static Katas _katas;
+
+        [SetUp]
+        public void Setup()
+        {
+            _katas = new Katas();
+        }
+
+        [Test]
+        public void Given_A_Sentence_When_Counting_Length_Of_Each_Word_Should_Return_ShortestWordLength()
+        {
+            //Arrange
+            var sentenceInput = "Njabulo is doing katas";
+            var expected = 2;
+            //Act
+            var actual = _katas.FindShort(sentenceInput);
+            //Asset
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Given_A_Sentence_When_Counting_Length_Of_Each_Word_Should_Return_ShortestWordLength_2()
+        {
+            //Arrange
+            var sentenceInput = "Njabulo is doing katas";
+            var expected = 2;
+            //Act
+            var actual = _katas.FindShortTwo(sentenceInput);
+            //Asset
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Given_A_Sentence_When_Counting_Length_Of_Each_Word_Should_Return_ShortestWordLength_Arrow()
+        {
+            //Arrange
+            var sentenceInput = "Njabulo is doing katas";
+            var expected = 2;
+            //Act
+            var actual = _katas.FindShortArrowMethod(sentenceInput);
+            //Asset
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Given_An_Array_When_FindingTheSumOfTwoNumbers_Should_Return_IndeciesOfTheTwoNumbers()
+        {
+            //Arrange
+            var inputArray = new[] { 1, 2, 3 };
+            var target = 4;
+            var expected = new[] { 0, 2};
+            //Act
+            var actual = _katas.TwoSum(inputArray,target);
+            //Asset
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Given_An_Array_When_FindingTheSumOfTwoNumbers_Should_Return_IndeciesOfTheTwoNumbers_2()
+        {
+            //Arrange
+            var inputArray = new[] { 1, 2, 3 };
+            var target = 4;
+            var expected = new[] { 0, 2 };
+            //Act
+            var actual = _katas.TwoSum(inputArray, target);
+            //Asset
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Given_An_Array_When_FindingTheSumOfTwoNumbers_Should_Return_IndeciesOfTheTwoNumbers_3()
+        {
+            //Arrange
+            var inputArray = new[] { 1234, 5678, 9012 };
+            var target = 14690;
+            var expected = new[] { 1, 2 };
+            //Act
+            var actual = _katas.TwoSum(inputArray, target);
+            //Asset
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
